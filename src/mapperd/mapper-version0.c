9994e8102258cff60d688cb76654f247bc217200
Filippos Giannakos <philipgian@grnet.gr>
2015-02-02 13:28:12 +0200

    mapper: Refactor v0 handling code
    Refactor v0 handling code to adjust it to the new internal run-time map
    representation.

--- src/mapperd/mapper-version0.c
+++ src/mapperd/mapper-version0.c
@@ -25,153 +25,112 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* version 0 functions */
 #define v0_chunked_read_size (512*1024)
 
-int read_object_v0(struct mapping *mn, unsigned char *buf)
+int read_object_v0(struct map *map, struct mapping *m, unsigned char *buf)
 {
-    hexlify(buf, SHA256_DIGEST_SIZE, mn->object);
-    mn->object[HEXLIFIED_SHA256_DIGEST_SIZE] = 0;
-    mn->objectlen = HEXLIFIED_SHA256_DIGEST_SIZE;
-    mn->flags = 0;              //not MF_OBJECT_WRITABLE;
-    //check if zero
-    if (!strncmp(mn->object, zero_block, ZERO_BLOCK_LEN)) {
-        mn->flags |= MF_OBJECT_ZERO;
+    int exists;
+    uint32_t i, cas_name_idx;
+    char cas_name[HEXLIFIED_SHA256_DIGEST_SIZE + 1];
+    hexlify(buf, SHA256_DIGEST_SIZE, cas_name);
+    cas_name[HEXLIFIED_SHA256_DIGEST_SIZE] = '\0';
+
+    //not MF_OBJECT_WRITABLE;
+    m->flags = 0;
+
+    if (!strncmp(cas_name, zero_block, ZERO_BLOCK_LEN)) {
+        m->flags = MF_OBJECT_ZERO;
+        return 0;
     }
+
+    exists = 0;
+    for (i = 0; i < map->cas_nr; i++) {
+        if (!memcmp(map->cas_array + i * HEXLIFIED_SHA256_DIGEST_SIZE,
+                    buf, HEXLIFIED_SHA256_DIGEST_SIZE)) {
+            exists = 1;
+            break;
+        }
+    }
+
+    if (exists) {
+        cas_name_idx = i;
+    } else {
+        // append cas_name_idx to cas_names;
+        memcpy(map->cas_array + map->cas_nr * HEXLIFIED_SHA256_DIGEST_SIZE,
+               buf, HEXLIFIED_SHA256_DIGEST_SIZE);
+        cas_name_idx = map->cas_nr;
+        map->cas_nr++;
+    }
+
+    m->name_idx = cas_name_idx;
+    m->vol_epoch = 0;
 
     return 0;
 }
 
-void object_to_map_v0(unsigned char *data, struct mapping *mn)
-{
-    unhexlify(mn->object, data);
-    //if name == zero block, raize MF_OBJECT_ZERO
-}
-
-struct xseg_request *prepare_write_object_v0(struct peer_req *pr,
-                                             struct map *map,
-                                             struct mapping *mn)
-{
-    struct peerd *peer = pr->peer;
-    struct mapperd *mapper = __get_mapperd(peer);
-    struct xseg_request *req;
-    char *data;
-    req = get_request(pr, mapper->mbportno, map->volume, map->volumelen,
-                      v0_objectsize_in_map);
-    if (!req) {
-        XSEGLOG2(&lc, E, "Cannot get request for map %s", map->volume);
-        return NULL;
-    }
-
-    req->op = X_WRITE;
-    req->size = v0_objectsize_in_map;
-    req->offset = v0_mapheader_size + mn->objectidx * v0_objectsize_in_map;
-
-    data = xseg_get_data(pr->peer->xseg, req);
-    object_to_map_v0((unsigned char *) data, mn);
-    return req;
-}
-
-int read_map_v0(struct map *m, unsigned char *data)
+int read_map_v0(struct map *map, unsigned char *data)
 {
     int r;
-    struct mapping *mapping;
-    uint64_t i;
-    uint64_t pos = 0, limit;
+    struct mapping *mapping = NULL;
+    uint64_t i, limit, pos = 0;
     uint64_t max_read_obj = v0_chunked_read_size / v0_objectsize_in_map;
     char nulls[SHA256_DIGEST_SIZE];
+    char *cas_array = NULL;
     memset(nulls, 0, SHA256_DIGEST_SIZE);
 
-    mapping = realloc(m->objects,
-                       (m->nr_objs + max_read_obj) * sizeof(struct mapping));
-    if (!mapping) {
-        return -1;
+    limit = map->nr_objs + max_read_obj;
+
+    // create enough space to hold CA objects names
+    cas_array = realloc(map->cas_array,
+                        limit * HEXLIFIED_SHA256_DIGEST_SIZE * sizeof(char));
+    if (!cas_array) {
+        return -ENOMEM;
     }
-    m->objects = mapping;
-    limit = m->nr_objs + max_read_obj;
-    for (i = m->nr_objs; i < limit; i++) {
+    map->cas_array = cas_array;
+
+    mapping = realloc(map->objects, limit * sizeof(struct mapping));
+    if (!mapping) {
+        // caller will clean up map->cas_array
+        return -ENOMEM;
+    }
+    map->objects = mapping;
+
+    for (i = map->nr_objs; i < limit; i++) {
         if (!memcmp(data + pos, nulls, v0_objectsize_in_map)) {
             break;
         }
-        mapping[i].objectidx = i;
-        mapping[i].map = m;
+        // Initialize mappings here incrementally, as we do not know the exact
+        // number of objects from the start.
         mapping[i].waiters = 0;
         mapping[i].state = 0;
         mapping[i].ref = 1;
         mapping[i].cond = st_cond_new();       //FIXME err check;
-        read_object_v0(&mapping[i], data + pos);
+        read_object_v0(map, &mapping[i], data + pos);
         pos += v0_objectsize_in_map;
     }
+
     XSEGLOG2(&lc, D, "Found %llu objects", i);
-    m->size = i * MAPPER_DEFAULT_BLOCKSIZE;
-    m->nr_objs = i;
-    return (limit - m->nr_objs);
+    map->size = i * MAPPER_DEFAULT_BLOCKSIZE;
+    map->nr_objs = i;
+
+    return (limit - map->nr_objs);
+}
+
+struct xseg_request *prepare_write_object_v0(struct peer_req *pr,
+                                             struct map *map,
+                                             uint64_t obj_idx,
+                                             struct mapping *mn)
+{
+    return NULL;
 }
 
 int delete_map_data_v0(struct peer_req *pr, struct map *map)
 {
-    return -1;
-    //perhaps use an X_DELETE ?
+    return -ENOTSUP;
 }
 
-struct xseg_request *__write_map_data_v0(struct peer_req *pr, struct map *map)
-{
-    int r;
-    struct peerd *peer = pr->peer;
-    struct mapperd *mapper = __get_mapperd(peer);
-    struct xseg_request *req;
-    char *data;
-    uint64_t datalen, pos, i;
-    struct mapping *mn;
-
-    datalen = v0_mapheader_size + map->nr_objs * v0_objectsize_in_map;
-    req = get_request(pr, mapper->mbportno, map->volume, map->volumelen,
-                      datalen);
-    if (!req) {
-        XSEGLOG2(&lc, E, "Cannot get request for map %s", map->volume);
-        goto out_err;
-    }
-
-
-    data = xseg_get_data(peer->xseg, req);
-
-    req->op = X_WRITE;
-    req->size = datalen;
-    req->offset = 0;
-
-    pos = 0;
-    for (i = 0; i < map->nr_objs; i++) {
-        mn = &map->objects[i];
-        object_to_map_v0((unsigned char *) (data + pos), mn);
-        pos += v0_objectsize_in_map;
-    }
-
-    r = send_request(pr, req);
-    if (r < 0) {
-        XSEGLOG2(&lc, E, "Cannot send request %p, pr: %p, map: %s",
-                 req, pr, map->volume);
-        goto out_put;
-    }
-
-    return req;
-
-  out_put:
-    put_request(pr, req);
-  out_err:
-    XSEGLOG2(&lc, E, "Map write for map %s failed.", map->volume);
-    return NULL;
-}
 
 int write_map_data_v0(struct peer_req *pr, struct map *map)
 {
-    int r = 0;
-    struct xseg_request *req = __write_map_data_v0(pr, map);
-    if (!req) {
-        return -1;
-    }
-    wait_on_pr(pr, (!(req->state & XS_FAILED || req->state & XS_SERVED)));
-    if (req->state & XS_FAILED) {
-        r = -1;
-    }
-    put_request(pr, req);
-    return r;
+    return -ENOTSUP;
 }
 
 
@@ -219,9 +178,26 @@ struct xseg_request *__load_map_data_v0(struct peer_req *pr, struct map *map)
 int load_map_data_v0(struct peer_req *pr, struct map *map)
 {
     int r = 0;
+    uint32_t i;
     struct xseg_request *req;
     struct peerd *peer = pr->peer;
     char *data;
+
+    // set defaults here
+    map->hex_cas_size = HEXLIFIED_SHA256_DIGEST_SIZE;
+    map->cur_vol_idx = 0;
+
+    map->vol_nr = 1;
+    map->vol_names = calloc(1, sizeof(struct vol_idx));
+    map->vol_array = calloc(map->volumelen, sizeof(char));
+    if (!map->vol_array || !map->vol_names) {
+        r = -ENOMEM;
+        goto out_restore;
+    }
+    memcpy(map->vol_array, map->volume, map->volumelen);
+    map->vol_names[0].len = map->volumelen;
+    map->vol_names[0].name = map->vol_array;
+
 
   retry:
     req = __load_map_data_v0(pr, map);
@@ -232,9 +208,11 @@ int load_map_data_v0(struct peer_req *pr, struct map *map)
 
     if (req->state & XS_FAILED) {
         XSEGLOG2(&lc, E, "Map load failed for map %s", map->volume);
+        r = -EIO;
         put_request(pr, req);
-        return -1;
+        goto out_restore;
     }
+
     //assert req->service == req->size
     data = xseg_get_data(peer->xseg, req);
     r = read_map_v0(map, (unsigned char *) data);
@@ -242,12 +220,30 @@ int load_map_data_v0(struct peer_req *pr, struct map *map)
     if (!r) {
         goto retry;
     }
+
+    if (r < 0) {
+        goto out_restore;
+    }
+
+    map->hex_cas_array_len = map->cas_nr * HEXLIFIED_SHA256_DIGEST_SIZE;
+    map->cas_names = calloc(map->cas_nr, sizeof(char *));
+    if (!map->cas_names) {
+        r = -ENOMEM;
+        goto out_restore;
+    }
+
+    for (i = 0; i < map->cas_nr; i++) {
+        map->cas_names[i] = map->cas_array + i * HEXLIFIED_SHA256_DIGEST_SIZE;
+    }
+
     return 0;
+
+out_restore:
+    restore_map_objects(map);
+    return r;
 }
 
 struct map_ops v0_ops = {
-    .object_to_map = object_to_map_v0,
-    .read_object = read_object_v0,
     .prepare_write_object = prepare_write_object_v0,
     .load_map_data = load_map_data_v0,
     .write_map_data = write_map_data_v0,
