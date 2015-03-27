8455f3777a0d62bff32c59b72f10ae994cca918d
Yiannis Tsiouris <gtsiour@softlab.ntua.gr>
2015-03-27 15:00:35 +0200

    Add init()/finalize() to header file

--- src/registerd/cr_api.h
+++ src/registerd/cr_api.h
@@ -1,3 +1,5 @@
+int init();
+int finalize();
 int register_client(int client_id, char *client_config_endpoint, char *client_info);
 int set_client_status(int client_id, client_status_t client_status);
 int get_client_info(int client_id);
